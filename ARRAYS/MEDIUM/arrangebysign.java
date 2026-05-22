c9fa1033fd033cbd2ef5a70a93bda8ec3eae336d
Chethana <chethanakoya@gmail>
2026-05-22 19:40:10 +0530

    added rearrange array by sign

---
+++ ARRAYS/MEDIUM/arrangebysign.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class arrangebysign {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+        for(int i=0; i<n;i++){
+            if(arr[i]>0){
+                pos.add(arr[i]);
+            }
+            else{
+                neg.add(arr[i]);
+            }
+        }
+        for(int i=0; i<n/2; i++){
+            arr[2*i] = pos.get(i);
+            arr[2*i+1] = neg.get(i);
+        }
+        System.out.println( Arrays.toString(arr) );
+    }
+}
